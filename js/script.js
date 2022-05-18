9c6e6be80aaabd1d9fe20096797eeea8605434e5
NikosDigalakis <nikos_23_chania@hotmail.com>
2022-05-18 17:58:51 +0200

    fork

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const url =
+  "https://raw.githubusercontent.com/bttmly/nba/master/data/teams.json";
+
+const results = document.querySelector(".results");
+
+async function getData() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    results.innerHTML = "";
+
+    for (i = 0; i < data.length; i++) {
+      if (i === 15) {
+        break;
+      }
+
+      if (
+        data[i].teamName.startsWith("C") ||
+        data[i].teamName.startsWith("c")
+      ) {
+        continue;
+      }
+      results.innerHTML += ` 
+      <div class="card"> <h3> ${data[i].teamName} </h3> 
+      <h4> ${data[i].location}
+      `;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getData();
